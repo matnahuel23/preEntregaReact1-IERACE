4486a0677976ffdb8b60ea0fee306f1c6d37e741
Matias Nahuel Ierace <matias.ierace@gmail.com>
2023-04-17 12:54:48 -0300

    creo NavBar para la primera pre entrega

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>MatNahuel Sports</h3>
+            <div>
+                <button>Zapatillas</button>
+                <button>Remeras</button>
+                <button>Pantalones</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
