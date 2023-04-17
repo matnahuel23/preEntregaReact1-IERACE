2af818923c1e3c3dc131520323a1a10483e9743b
Matias Nahuel Ierace <matias.ierace@gmail.com>
2023-04-17 13:11:15 -0300

    creo ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      {/* Invoco itemlistcontainer debajo del navbar */}
+      {/* abajo la '' es la del signo de ? */}
+      <ItemListContainer greeting={'Bienvenido!'}/> 
     </div>
   );
 }
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,9 @@
+const ItemListContainer = ({greeting}) => { //greeting es el Saludo inicial con mis estilos
+    return (
+    <div>
+        <h1>{greeting}</h1>
+    </div>
+    )
+}
+
+export default ItemListContainer
