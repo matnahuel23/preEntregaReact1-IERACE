4a66163e660342da8fbcfbfe689830d2e3d1bf08
Matias Nahuel Ierace <matias.ierace@gmail.com>
2023-04-17 14:50:20 -0300

    introduzco bootstrap

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,34 @@
-import CartWidget from "../CartWidget/CartWidget"
+import CartWidget from "../CartWidget/CartWidget";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 const NavBar = () => {
     return (
-        <nav>
-            <h3>MatNahuel Sports</h3>
-            <div>
-                <button>Zapatillas</button>
-                <button>Remeras</button>
-                <button>Pantalones</button>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">MatNahuel Sports</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Principal</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Sexo
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Masculino</a></li>
+                                <li><a class="dropdown-item" href="#">Femenino</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
             <CartWidget />
-        </nav>
+        </nav>       
     )
 }
 
